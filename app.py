11af07529704033b9017ea4a4dccb9cedb8c6726
AstroReza <zrezafauzan@gmail.com>
2026-05-17 07:44:34 +0700

    Resolve conflict

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ def index():
     # Renders the index.html file from the 'templates' folder
     return render_template('index.html')
 
+@app.route('/orbit-link')
+def orbit_link():
+    # Renders the orbit-link.html file from the 'templates' folder
+    return render_template('orbit-link.html')
+
 if __name__ == '__main__':
     # Run the app in debug mode on port 5000
     app.run(debug=True, port=5000)
